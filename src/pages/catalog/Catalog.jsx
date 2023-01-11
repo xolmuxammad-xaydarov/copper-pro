8d9109c832faa23e1644cfa4e9d9c928f8812688
rayyanabahadirova <reyyanmebahadirovaa@gmail.com>
2023-01-10 18:45:08 -0900

    Rayyana

--- src/pages/catalog/Catalog.jsx
+++ src/pages/catalog/Catalog.jsx
@@ -3,7 +3,147 @@ import './Catalog.scss'
 
 function Catalog() {
   return (
-    <h1>Catalog</h1>
+    <>
+        <div className="App">
+
+        <div className="container">
+
+            <p id="katalog1">Главная / <span>Каталог</span></p>
+            <p id="katalog2">Каталог</p>         
+          <div id="cards_one_wrapper">
+
+              <div id="card">
+                    
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpz3fBGdZZsTivUPWt7MQm8plG6-RssFRRTw&usqp=CAU' alt="" />
+                    <p>Для эфирных масел</p>
+              </div>
+              
+              <div id="card">
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpz3fBGdZZsTivUPWt7MQm8plG6-RssFRRTw&usqp=CAU' alt="" />
+                <p>Для эфирных масел</p>
+              </div>
+
+              <div id="card">
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpz3fBGdZZsTivUPWt7MQm8plG6-RssFRRTw&usqp=CAU' alt="" />
+                <p>Для эфирных масел</p>
+              </div>
+
+              <div id="card" >
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpz3fBGdZZsTivUPWt7MQm8plG6-RssFRRTw&usqp=CAU' alt="" />
+                <p>Для эфирных масел</p>
+              </div>
+              
+          </div>
+              <div className="search_wrapper">
+                      <div id="search">
+                      <input id='inp' type="text" placeholder='Поиск' /> <i class="fa-solid fa-magnifying-glass"></i>
+                      </div>
+                      <p>Сортировка: <span>По цене</span> <i class="fa-solid fa-chevron-down"></i></p>
+                  </div>
+
+                  <div className="cards__wrapper_two">
+                      <div className="container__wrapper">
+                            <div className="heart">
+                                <i class="fa-regular fa-heart"></i>
+                            </div>
+                            <img src="https://previews.123rf.com/images/vizafoto/vizafoto1802/vizafoto180200459/103667762-old-silver-dishes-with-accessories-for-the-kitchen.jpg" alt="" />
+                            <div className="img_text">
+                                <div>Медный чайник с фарфоровой ручкой</div> <p>1 953 грн</p>
+                            </div>
+                      </div>
+
+                      <div className="container__wrapper">
+                        <div className="heart">
+                                <i class="fa-regular fa-heart"></i>
+                            </div>
+                      <img src="https://previews.123rf.com/images/vizafoto/vizafoto1802/vizafoto180200459/103667762-old-silver-dishes-with-accessories-for-the-kitchen.jpg" alt="" />
+                      <div className="img_text">
+                          <div>Медный чайник с фарфоровой ручкой</div> <p>1 953 грн</p>
+                      </div>
+                      </div>
+
+                      <div className="container__wrapper">
+                        <div className="heart">
+                                <i class="fa-regular fa-heart"></i>
+                            </div>
+                      <img src="https://previews.123rf.com/images/vizafoto/vizafoto1802/vizafoto180200459/103667762-old-silver-dishes-with-accessories-for-the-kitchen.jpg" alt="" />
+                      <div className="img_text">
+                          <div>Медный чайник с фарфоровой ручкой</div> <p>1 953 грн</p>
+                      </div>
+                      </div>
+
+                      <div className="container__wrapper">
+                        <div className="heart">
+                                <i class="fa-regular fa-heart"></i>
+                            </div>
+                      <img src="https://previews.123rf.com/images/vizafoto/vizafoto1802/vizafoto180200459/103667762-old-silver-dishes-with-accessories-for-the-kitchen.jpg" alt="" />
+                      <div className="img_text">
+                          <div>Медный чайник с фарфоровой ручкой</div> <p>1 953 грн</p>
+                      </div>
+                      </div>
+
+                      <div className="container__wrapper">
+                        <div className="heart">
+                                <i class="fa-regular fa-heart"></i>
+                            </div>
+                      <img src="https://previews.123rf.com/images/vizafoto/vizafoto1802/vizafoto180200459/103667762-old-silver-dishes-with-accessories-for-the-kitchen.jpg" alt="" />
+                      <div className="img_text">
+                          <div>Медный чайник с фарфоровой ручкой</div> <p>1 953 грн</p>
+                      </div>
+                      </div>
+
+                      <div className="container__wrapper">
+                        <div className="heart">
+                                <i class="fa-regular fa-heart"></i>
+                            </div>
+                      <img src="https://previews.123rf.com/images/vizafoto/vizafoto1802/vizafoto180200459/103667762-old-silver-dishes-with-accessories-for-the-kitchen.jpg" alt="" />
+                      <div className="img_text">
+                          <div>Медный чайник с фарфоровой ручкой</div> <p>1 953 грн</p>
+                      </div>
+                      </div>
+
+                      <div className="container__wrapper">
+                        <div className="heart">
+                                <i class="fa-regular fa-heart"></i>
+                            </div>
+                      <img src="https://previews.123rf.com/images/vizafoto/vizafoto1802/vizafoto180200459/103667762-old-silver-dishes-with-accessories-for-the-kitchen.jpg" alt="" />
+                      <div className="img_text">
+                          <div>Медный чайник с фарфоровой ручкой</div> <p>1 953 грн</p>
+                      </div>
+                      </div>
+
+                      <div className="container__wrapper">
+                        <div className="heart">
+                                <i class="fa-regular fa-heart"></i>
+                            </div>
+                      <img src="https://previews.123rf.com/images/vizafoto/vizafoto1802/vizafoto180200459/103667762-old-silver-dishes-with-accessories-for-the-kitchen.jpg" alt="" />
+                      <div className="img_text">
+                          <div>Медный чайник с фарфоровой ручкой</div> <p>1 953 грн</p>
+                      </div>
+                      </div>
+
+                      <div className="container__wrapper">
+                        <div className="heart">
+                                <i class="fa-regular fa-heart"></i>
+                            </div>
+                      <img src="https://previews.123rf.com/images/vizafoto/vizafoto1802/vizafoto180200459/103667762-old-silver-dishes-with-accessories-for-the-kitchen.jpg" alt="" />
+                      <div className="img_text">
+                          <div>Медный чайник с фарфоровой ручкой</div> <p>1 953 грн</p>
+                      </div>
+                      </div>
+                  </div>
+                  <div className="slider__wrapper1">
+                        <div className="one"><i class="fa-solid fa-chevron-left"></i></div>
+                        <div className="two">1</div>
+                        <div className="three">2</div>
+                        <div className="four">3</div>
+                        <div className="five">4</div>
+                        <div className="six"><i class="fa-solid fa-chevron-right"></i></div>
+                  </div>
+        </div>
+
+    </div>
+    </>
   )
 }
 
